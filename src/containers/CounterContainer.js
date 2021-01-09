20ac658d1d9c5a8ff6add5b56c1f0e47802b8cfe
Keu-ni <skcor8909@gmail.com>
2021-01-09 19:35:19 +0900

    use connect function

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import Counter from '../components/Counter';
-import { useSelector, useDispatch, shallowEqual } from 'react-redux';
+import { connect } from 'react-redux';
 import { decrease, increase, setDiff } from '../modules/counter';
 
-function CounterContainer() {
-  const { number, diff } = useSelector(
-    (state) => ({
-      number: state.counter.number,
-      diff: state.counter.diff,
-    }),
-    shallowEqual
-  );
-
-  const dispatch = useDispatch();
-
-  const onIncrease = () => dispatch(increase());
-  const onDecrease = () => dispatch(decrease());
-  const onSetDiff = (diff) => dispatch(setDiff(diff));
-
+function CounterContainer({ number, diff, increase, decrease, setDiff }) {
   return (
     <Counter
       number={number}
       diff={diff}
-      onIncrease={onIncrease}
-      onDecrease={onDecrease}
-      onSetDiff={onSetDiff}
+      onIncrease={increase}
+      onDecrease={decrease}
+      onSetDiff={setDiff}
     />
   );
 }
-export default CounterContainer;
+
+
+// mapStateToProps 는 리덕스 스토어의 상태를 조회해서 어떤 것들을 props 로 넣어줄지 정의합니다.
+// 현재 리덕스 상태를 파라미터로 받아옵니다.
+const mapStateToProps = (state) => ({
+  number: state.counter.number,
+  diff: state.counter.diff,
+});
+
+
+// mapDispatchToProps가 함수가 아니라 객체면
+// bindActionCreators 를 connect 에서 대신 해줍니다.
+const mapDispatchToProps = {
+  increase,
+  decrease,
+  setDiff,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);

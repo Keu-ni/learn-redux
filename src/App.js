0a7de18b6a7380699369fdbb1f459e98c6c4ddb3
Keu-ni <skcor8909@gmail.com>
2021-01-10 15:07:26 +0900

    add postList Component, Container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import CounterContainer from "./containers/CounterContainer";
+import CounterContainer from './containers/CounterContainer';
+import PostListContainer from './containers/PostListContainer';
 
 function App() {
   return (
-    <CounterContainer />
+    <>
+      <CounterContainer />
+      <hr />
+      <PostListContainer />
+    </>
   );
 }
 

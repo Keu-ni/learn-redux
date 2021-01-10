fa194946bc3c431638bf6ddf1015f656e4115338
Keu-ni <skcor8909@gmail.com>
2021-01-10 14:52:02 +0900

    posts module is written rootReducer

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import counter from './counter';
+import posts from './post'
 
-const rootReducer = combineReducers({ counter });
+const rootReducer = combineReducers({ counter, posts });
 
 export default rootReducer;
